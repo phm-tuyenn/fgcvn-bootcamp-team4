cb2e8b36d96b1c1419b92018be0890a4dd2ed9a4
phm-tuyenn <phamthanhtuyen2k8@gmail.com>
2024-06-11 15:30:30 +0700

    Add Idea 3 and update README.md

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpIdea1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpIdea1.java
@@ -8,12 +8,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "MainOpIdea1")
 public class MainOpIdea1 {
+    //define hardware
     private DcMotorEx leftMotor, rightMotor;
     private DcMotorEx climbMotor, armControlMotor;
-
+    private Servo deployArmServo;
+    //drivetrain: 2 joystick for arcade
     private void drive() {
         double leftSpeed = gamepad1.left_stick_y + gamepad1.left_stick_x;
         double rightSpeed = gamepad1.left_stick_y - gamepad1.left_stick_x;
@@ -21,16 +24,26 @@ public class MainOpIdea1 {
         leftMotor.setPower(leftSpeed);
         rightMotor.setPower(rightSpeed);
     }
-
+    //climb
     private void climb() {
-        climbMotor.setPower(0);
-        armControlMotor.setPower(0);
+        //make motor stall if no button is pressed
+        if(!gamepad1.a && !gamepad1.b) climbMotor.setPower(0);
+        if(!gamepad1.x && !gamepad1.y) armControlMotor.setPower(0);
+        //deploy linear: button start for release, back for reset
+        if(gamepad1.start) {
+            deployArmServo.setPosition(1);
+        }
+        if(gamepad1.back) {
+            deployArmServo.setPosition(-1);
+        }
+        //pull the robot up/down: button A for pull up, B for down
         if(gamepad1.a) {
             climbMotor.setPower(1);
         }
         if(gamepad1.b) {
             climbMotor.setPower(-1);
         }
+        //change the angle of linear arm: button X for rotate right, Y for left
         if(gamepad1.x) {
             armControlMotor.setPower(0.3);
         }
@@ -41,17 +54,23 @@ public class MainOpIdea1 {
 
     @Override
     public void runOpMode() {
+        //drivetrain motor
         leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
         rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+        //climbing motor & servo
         climbMotor = hardwareMap.get(DcMotorEx.class, "climbMotor");
         armControlMotor = hardwareMap.get(DcMotorEx.class, "armControlMotor");
-
+        deployArmServo = hardwareMap.get(Servo.class, "deployArmServo");
+        //set reverse for rightMotor in drivetrain and brake for motor in climbing mechanism
         rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         climbMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armControlMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         linearOpMode.waitForStart();
         if(linearOpMode.opModeIsActive()) {
+            //set initial position for servo
+            deployArmServo.setPosition(-1);
+            //main loop
             while(linearOpMode.opModeIsActive()) {
                 drive();
                 climb();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpIdea2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpIdea2.java
@@ -12,10 +12,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "MainOpIdea2")
 public class MainOpIdea2 {
+    //define hardware
     private DcMotorEx leftMotor, rightMotor;
     private DcMotorEx climbMotor;
     private Servo deployArmServo;
-
+    //drivetrain: 2 joystick for arcade
     private void drive() {
         double leftSpeed = gamepad1.left_stick_y + gamepad1.left_stick_x;
         double rightSpeed = gamepad1.left_stick_y - gamepad1.left_stick_x;
@@ -23,32 +24,44 @@ public class MainOpIdea2 {
         leftMotor.setPower(leftSpeed);
         rightMotor.setPower(rightSpeed);
     }
-
+    //climb
     private void climb() {
+        //stall motor if no button is pressed
+        if(!gamepad1.a && !gamepad1.b) climbMotor.setPower(0);
+        //deploy the linear: start for release, back for reset
+        if(gamepad1.start) {
+            deployArmServo.setPosition(1);
+        }
+        if(gamepad1.back) {
+            deployArmServo.setPosition(-1);
+        }
+        //pull the robot up/down: A for up, B for down
         if(gamepad1.a) {
             climbMotor.setPower(1);
         }
         if(gamepad1.b) {
             climbMotor.setPower(-1);
         }
-        if(gamepad1.x) {
-            deployArmServo.setPosition(1);
-        }
+
     }
 
     @Override
     public void runOpMode() {
+        //drivetrain motor
         leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
         rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+        //climbing motor & servo
         climbMotor = hardwareMap.get(DcMotorEx.class, "climbMotor");
         deployArmServo = hardwareMap.get(Servo.class, "deployArmServo");
-
+        //set reverse for rightMotor in drivetrain and brake for climbMotor
         rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         climbMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         linearOpMode.waitForStart();
         if(linearOpMode.opModeIsActive()) {
+            //init deploy servo
             deployArmServo.setPosition(-1);
+            //main loop
             while(linearOpMode.opModeIsActive()) {
                 drive();
                 climb();
